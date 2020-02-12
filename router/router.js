d7fe89e58c31d49e5c8556110bfb8410a39315e3
Brennon Nuckols <brennuck@gmail.com>
2020-02-12 13:36:02 -0700

    PUT

--- router/router.js
+++ router/router.js
@@ -57,39 +57,64 @@ router.post("/", (req, res) => {
   Posts.insert(postData)
     .then(post => {
       if (!postData.title || !postData.contents) {
-        res
-          .status(404)
-          .json({
-            errorMessage: "Please provide title and contents for the post."
-          });
+        res.status(404).json({
+          errorMessage: "Please provide title and contents for the post."
+        });
       } else {
         res.status(201).json(post);
       }
     })
     .catch(error => {
       console.log(error);
-      res
-        .status(500)
-        .json({
-          error: "There was an error while saving the post to the database"
-        });
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
     });
 });
 
-router.delete('/:id', (req, res) => {
-    const id = req.params.id
-    Posts.remove(id)
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Posts.remove(id)
     .then(post => {
-        if (!post) {
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
-        } else {
-            res.status(200).json(post)
-        }
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200).json(post);
+      }
     })
     .catch(error => {
-        console.log(error)
-        res.status(500).json({ error: "The post could not be removed" })
+      console.log(error);
+      res.status(500).json({ error: "The post could not be removed" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  Posts.update(id, changes)
+    .then(post => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else if (!changes.title || !changes.contents) {
+        res
+          .status(400)
+          .json({
+            errorMessage: "Please provide title and contents for the post."
+          });
+      } else {
+        res.status(200).json(post);
+      }
     })
-})
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be modified." });
+    });
+});
 
 module.exports = router;
